3cc495d10c6b11339c959010792fd463aaaa13e7
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-09-29 13:56:49 +0300

    fixed bug of avatar in user menu

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -34,7 +34,7 @@ export const UserMenu = ({name, email}) => {
         <Box sx={{ flexGrow: 0 }}>
           <Tooltip title="Open settings">
             <IconButton onClick={handleOpenUserMenu} sx={{ p: 1 }}>
-              <Avatar alt={name} src="/static/images/avatar/2.jpg" />
+              <Avatar alt={name} src="" />
             </IconButton>
           </Tooltip>
           <Menu
